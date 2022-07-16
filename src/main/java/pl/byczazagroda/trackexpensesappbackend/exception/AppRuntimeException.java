98376c6c680d6ef251f6ce314a9cc93bde8f7755
Jakub Kupczyk <jkbkupczyk@gmail.com>
2022-07-16 19:58:58 +0200

    Create basic App exceptions

---
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/exception/AppRuntimeException.java
@@ -0,0 +1,20 @@
+package pl.byczazagroda.trackexpensesappbackend.exception;
+
+public class AppRuntimeException extends RuntimeException {
+
+    public AppRuntimeException(String message) {
+        super(message);
+    }
+
+    public AppRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AppRuntimeException(Throwable cause) {
+        super(cause);
+    }
+
+    public AppRuntimeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
