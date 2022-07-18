773aa05267a52e530a1aa3e8737cf7b421cf20d9
Mateusz Bednarczyk <91325367+MateuszBednarczyk@users.noreply.github.com>
2022-07-18 13:46:38 +0200

    Feature: Added exception handler controller

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/exception/GlobalExceptionHandlerController.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/exception/GlobalExceptionHandlerController.java
@@ -1,4 +1,4 @@
-package pl.byczazagroda.trackexpensesappbackend.controller;
+package pl.byczazagroda.trackexpensesappbackend.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
