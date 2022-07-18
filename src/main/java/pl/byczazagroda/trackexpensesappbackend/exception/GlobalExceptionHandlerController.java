72a86fb6a7b4ec940f53243137966ac66ac7aca0
Mateusz Bednarczyk <91325367+MateuszBednarczyk@users.noreply.github.com>
2022-07-18 13:53:16 +0200

    Feature: Added exception handler controller (#13)
    * Feature: Added exception handler controller
    
    * Feature: Added exception handler controller

---
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/exception/GlobalExceptionHandlerController.java
@@ -0,0 +1,36 @@
+package pl.byczazagroda.trackexpensesappbackend.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import pl.byczazagroda.trackexpensesappbackend.exception.AppException;
+import pl.byczazagroda.trackexpensesappbackend.exception.AppRuntimeException;
+
+@Slf4j
+@RestControllerAdvice
+class GlobalExceptionHandlerController {
+
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String catchThrowableException(Throwable e) {
+        log.error("Throwable", e);
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(AppException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String catchAppException(AppException e) {
+        log.error("AppException", e);
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(AppRuntimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String catchAppRuntimeException(AppRuntimeException e) {
+        log.error("AppRuntimeException", e);
+        return e.getMessage();
+    }
+
+}
