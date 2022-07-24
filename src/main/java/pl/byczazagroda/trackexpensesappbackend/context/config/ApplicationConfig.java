452c0df4235f158090c067f99bd0c73d19cfdd8f
Dawid Bielecki dawciobiel <dawciobiel@gmail.com>
2022-07-25 00:31:00 +0200

    Created application config to store project and @Bean configurations. Configured ObjectMapper to ignore NULL values. Added documentation and code formatting.

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/context/config/ApplicationConfig.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/context/config/ApplicationConfig.java
@@ -4,17 +4,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
-@EnableAsync
-@EnableCaching
-@EnableScheduling
-@EnableAspectJAutoProxy
 @RequiredArgsConstructor
 public class ApplicationConfig {
 
---
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/context/config/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides application configuration settings
+ *
+ * @since 1.0
+ * @version 1.1
+ */
+package pl.byczazagroda.trackexpensesappbackend.context.config;
