4fba292c1592166d5d75678c4040a7b5644d49e4
dawciobiel <githubhL5p4Cf>
2022-08-20 02:18:15 +0200

    Added Response Service to Controller Protocol (RSCP) package with package description

---
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/rscp/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This module applies to classes, interfaces or enumeration for Response Service to Controller Protocol (RSCP).<br>
+ * What is used for communication from any service to other service or service to controller.
+ *
+ * @since 0.2
+ * @author Dawid Bielecki dawciobiel
+ * @version 1.0
+ */
+package pl.byczazagroda.trackexpensesappbackend.rscp;
