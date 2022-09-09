caad1209f3d7eb6078d6aba99db5cc66d835703a
Dawid Stużyński <dawid.stuzynski@gmail.com>
2022-09-09 22:04:58 +0200

    Remove @Immutable annotation from WalletDTO
    @Immutable is hibernate annotation and it is not needed in DT
    O class

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/dto/WalletDTO.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/dto/WalletDTO.java
@@ -1,11 +1,8 @@
 package pl.byczazagroda.trackexpensesappbackend.dto;
 
-import org.hibernate.annotations.Immutable;
-
 import java.time.Instant;
 
 
-@Immutable
 public record WalletDTO(Long id, String name, Instant creationDate) {
 
 }
