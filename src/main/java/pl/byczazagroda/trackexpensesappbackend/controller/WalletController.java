8847ce270011c2dd41cd70647c37ef3f06ff612c
Dawid Stużyński <dawid.stuzynski@gmail.com>
2022-09-09 15:32:30 +0200

    Add WalletController

---
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/controller/WalletController.java
@@ -0,0 +1,15 @@
+package pl.byczazagroda.trackexpensesappbackend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.byczazagroda.trackexpensesappbackend.service.WalletService;
+
+@RestController
+@RequestMapping("api/wallet")
+@RequiredArgsConstructor
+public class WalletController {
+
+    private final WalletService walletService;
+
+}
