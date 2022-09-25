e2ed6f5433c0d94bd38fea95ff6a1ac480e2ae55
Dawid Stużyński <dawid.stuzynski@gmail.com>
2022-09-25 17:13:29 +0200

    feat: Implement createWallet method

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/service/WalletServiceImpl.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/service/WalletServiceImpl.java
@@ -2,12 +2,34 @@ package pl.byczazagroda.trackexpensesappbackend.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.byczazagroda.trackexpensesappbackend.dto.CreateWalletDTO;
+import pl.byczazagroda.trackexpensesappbackend.dto.WalletDTO;
+import pl.byczazagroda.trackexpensesappbackend.exception.WalletNotSavedException;
+import pl.byczazagroda.trackexpensesappbackend.mapper.WalletModelMapper;
+import pl.byczazagroda.trackexpensesappbackend.model.Wallet;
 import pl.byczazagroda.trackexpensesappbackend.repository.WalletRepository;
+
+import java.time.Instant;
 
 @Service
 @RequiredArgsConstructor
 public class WalletServiceImpl implements WalletService {
 
     private final WalletRepository walletRepository;
+    private final WalletModelMapper walletModelMapper;
 
+
+    @Override
+    public WalletDTO createWallet(CreateWalletDTO createWalletDTO) {
+        String walletName = createWalletDTO.name();
+        Instant creationTime = createWalletDTO.creationTime();
+        Wallet wallet = new Wallet(walletName, creationTime);
+        Wallet savedWallet = walletRepository.save(wallet);
+        boolean isWalletExists = walletRepository.existsById(savedWallet.getId());
+
+        if (isWalletExists) {
+            return walletModelMapper.mapWalletEntityToWalletDTO(savedWallet);
+        }
+        throw new WalletNotSavedException();
+    }
 }
