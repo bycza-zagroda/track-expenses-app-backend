341f375f4ea1230f5161a9c7c1ffd3bd75139908
Dawid Stużyński <dawid.stuzynski@gmail.com>
2022-09-25 17:15:46 +0200

    feat: Add createWallet and walletNotSavedHandler methods

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/controller/WalletController.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/controller/WalletController.java
@@ -1,8 +1,14 @@
 package pl.byczazagroda.trackexpensesappbackend.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.byczazagroda.trackexpensesappbackend.dto.CreateWalletDTO;
+import pl.byczazagroda.trackexpensesappbackend.dto.WalletDTO;
+import pl.byczazagroda.trackexpensesappbackend.exception.AppRuntimeException;
+import pl.byczazagroda.trackexpensesappbackend.exception.WalletNotSavedException;
 import pl.byczazagroda.trackexpensesappbackend.service.WalletService;
 
 /**
@@ -14,4 +20,23 @@ import pl.byczazagroda.trackexpensesappbackend.service.WalletService;
 public class WalletController {
 
     private final WalletService walletService;
+
+    @PostMapping("/create")
+    public ResponseEntity<WalletDTO> createWallet(@RequestBody CreateWalletDTO createWalletDTO) {
+
+        WalletDTO walletDTO = walletService.createWallet(createWalletDTO);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("message", "You have successfully completed the creation of a new Wallet!");
+
+        return new ResponseEntity<>(walletDTO, headers, HttpStatus.CREATED);
+    }
+
+    @ExceptionHandler(WalletNotSavedException.class)
+    public ResponseEntity<AppRuntimeException> walletNotSavedHandler(WalletNotSavedException exception) {
+
+        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        return new ResponseEntity<>(exception, httpStatus);
+
+    }
 }
