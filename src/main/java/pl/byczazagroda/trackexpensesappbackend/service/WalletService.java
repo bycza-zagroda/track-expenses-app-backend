ba3241fd6587ae8581dee28bee4bf61857fb363b
Krystian <krystian14d@gmail.com>
2022-09-30 17:53:07 +0200

    update WalletService

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/service/WalletService.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/service/WalletService.java
@@ -1,8 +1,9 @@
 package pl.byczazagroda.trackexpensesappbackend.service;
 
+import pl.byczazagroda.trackexpensesappbackend.dto.EditWalletDto;
 import pl.byczazagroda.trackexpensesappbackend.dto.WalletDTO;
 
 public interface WalletService {
 
-    WalletDTO updateWallet(long id, String name);
+    WalletDTO updateWallet(EditWalletDto walletToEdit);
 }
--- src/main/java/pl/byczazagroda/trackexpensesappbackend/service/WalletServiceImpl.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/service/WalletServiceImpl.java
@@ -3,6 +3,7 @@ package pl.byczazagroda.trackexpensesappbackend.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+import pl.byczazagroda.trackexpensesappbackend.dto.EditWalletDto;
 import pl.byczazagroda.trackexpensesappbackend.dto.WalletDTO;
 import pl.byczazagroda.trackexpensesappbackend.dto.WalletModelMapper;
 import pl.byczazagroda.trackexpensesappbackend.exception.WalletNotFoundException;
@@ -10,11 +11,6 @@ import pl.byczazagroda.trackexpensesappbackend.model.Wallet;
 import pl.byczazagroda.trackexpensesappbackend.repository.WalletRepository;
 
 import javax.transaction.Transactional;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 @Primary
 @Service
@@ -24,22 +20,16 @@ public class WalletServiceImpl implements WalletService {
     private final WalletRepository walletRepository;
     private final WalletModelMapper mapper;
 
-    //TODO: czy walidacja w serwisie też potrzebna? jeśli tak to co z @ExceptionHandler?
-    //TODO: czy rzucanie tutaj wyjątku WalletNotFoundException jest OK? czy trzeba koniecznie do kontrolera?
-    //TODO: czy mapowanie z użyciem MapStruct jest OK?
     @Transactional
-    public WalletDTO updateWallet(@NotNull @Min(value = 1) long id,
-                                  @NotNull @NotEmpty @Size(max = 20) @Pattern(regexp = "[a-z A-Z]+") String name){
-        Wallet wallet = walletRepository.findById(id).orElseThrow(() ->
+    public WalletDTO updateWallet(EditWalletDto walletToEdit) {
+        Wallet wallet = walletRepository.findById(walletToEdit.id()).orElseThrow(() ->
         {
-            throw new WalletNotFoundException(String.format("Wallet with given ID: %s does not exist", id));
+            throw new WalletNotFoundException(String.format("Wallet with given ID: %s does not exist", walletToEdit.id()));
         });
 
-        wallet.setName(name);
-        //TODO: czy potrzebujemy tutaj zapisywać encję?
-        walletRepository.save(wallet);
+        wallet.setName(walletToEdit.name());
 
-        return mapper.toDto(wallet);
+        return mapper.mapWalletEntityToWalletDTO(wallet);
     }
 
 }
