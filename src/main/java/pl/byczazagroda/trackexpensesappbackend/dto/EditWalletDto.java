e4e821ec34afac1a3b95e444e43acfa27e443b02
Krystian <krystian14d@gmail.com>
2022-09-30 17:43:38 +0200

    create EditWalletDto class

---
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/dto/EditWalletDto.java
@@ -0,0 +1,11 @@
+package pl.byczazagroda.trackexpensesappbackend.dto;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public record EditWalletDto(@NotNull @NotEmpty @Min(value = 1) long id, @NotBlank @NotEmpty @Size(max = 20) @Pattern(regexp = "[a-z A-Z]+") String name) {
+}
