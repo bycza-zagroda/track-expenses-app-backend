b505db375feb1edc12e7ab2ffd37f24042b36486
Dawid Stużyński <dawid.stuzynski@gmail.com>
2022-10-01 21:39:10 +0200

    refactor: Add @ActiveProfiles(test) annotation to BaseControllerTest

--- src/test/java/pl/byczazagroda/trackexpensesappbackend/BaseControllerTest.java
+++ src/test/java/pl/byczazagroda/trackexpensesappbackend/BaseControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,6 +16,7 @@ import org.testcontainers.utility.DockerImageName;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Testcontainers
+@ActiveProfiles("test")
 public abstract class BaseControllerTest {
 
     @Autowired
