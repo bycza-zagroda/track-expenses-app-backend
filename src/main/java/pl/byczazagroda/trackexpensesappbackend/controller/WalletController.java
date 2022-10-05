b43e089d300833508e910fe8d3a0f914cc3591b8
Dawid Stużyński <dawid.stuzynski@gmail.com>
2022-10-05 20:23:17 +0200

    feat: Add handleMethodArgumentNotValidException to WalletController

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/controller/WalletController.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/controller/WalletController.java
@@ -4,12 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import pl.byczazagroda.trackexpensesappbackend.dto.CreateWalletDTO;
 import pl.byczazagroda.trackexpensesappbackend.dto.WalletDTO;
 import pl.byczazagroda.trackexpensesappbackend.exception.AppRuntimeException;
 import pl.byczazagroda.trackexpensesappbackend.exception.WalletNotSavedException;
 import pl.byczazagroda.trackexpensesappbackend.service.WalletService;
+
+import javax.validation.Valid;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Controller for Wallet application.
@@ -22,7 +28,7 @@ public class WalletController {
     private final WalletService walletService;
 
     @PostMapping("/create")
-    public ResponseEntity<WalletDTO> createWallet(@RequestBody CreateWalletDTO createWalletDTO) {
+    public ResponseEntity<WalletDTO> createWallet(@Valid @RequestBody CreateWalletDTO createWalletDTO) {
 
         WalletDTO walletDTO = walletService.createWallet(createWalletDTO);
         HttpHeaders headers = new HttpHeaders();
@@ -37,6 +43,13 @@ public class WalletController {
         HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
 
         return new ResponseEntity<>(exception, httpStatus);
+    }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        return ex.getBindingResult().getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
     }
 }
