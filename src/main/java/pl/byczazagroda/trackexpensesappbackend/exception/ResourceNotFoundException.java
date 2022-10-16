b8772899a9a36a84a049c2fbeadac983b8ebbf7b
Krystian <krystian14d@gmail.com>
2022-10-16 19:00:08 +0200

    feat: Method to update single wallet
    -refactor ResourceNotFoundException

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/exception/ResourceNotFoundException.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/exception/ResourceNotFoundException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Wallet not found")
-public class WalletNotFoundException extends AppRuntimeException {
-    public WalletNotFoundException(String message) {
+public class ResourceNotFoundException extends AppRuntimeException {
+    public ResourceNotFoundException(String message) {
         super(message);
     }
 }
