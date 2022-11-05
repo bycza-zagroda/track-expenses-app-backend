a7611067ac43598b46372d40aa24f0519426fd70
Iwona007 <iwonka.adamkiewicz@gmail.com>
2022-11-05 20:07:44 +0100

    Merge branch 'refactoring' of https://github.com/Iwona007/track-expenses-app-backend into Iwona007-refactoring
    # Conflicts:
    #	src/main/java/pl/byczazagroda/trackexpensesappbackend/controller/WalletController.java
    #	src/main/java/pl/byczazagroda/trackexpensesappbackend/exception/WalletExceptionHandler.java
    #	src/main/java/pl/byczazagroda/trackexpensesappbackend/service/WalletService.java
    #	src/main/java/pl/byczazagroda/trackexpensesappbackend/service/WalletServiceImpl.java
    #	src/test/java/pl/byczazagroda/trackexpensesappbackend/controller/WalletControllerTest.java
    #	src/test/java/pl/byczazagroda/trackexpensesappbackend/service/WalletServiceImplTest.java

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/exception/GlobalExceptionHandlerController.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/exception/GlobalExceptionHandlerController.java
@@ -67,76 +67,6 @@ class GlobalExceptionHandlerController extends ResponseEntityExceptionHandler {
                 HttpStatus.BAD_REQUEST);
     }
 
-    //    @ExceptionHandler(Throwable.class)
-//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-//    public String catchThrowableException(Throwable e) {
-//        log.error("Throwable", e);
-//        return e.getMessage();
-//    }
-
-//    @ExceptionHandler(AppRuntimeException.class)
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    public String catchAppRuntimeException(AppRuntimeException e) {
-//        log.error("AppRuntimeException", e);
-//        return e.getMessage();
-//    }
-
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    public String handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-//        e.getBindingResult().getFieldErrors().forEach(m ->
-//                log.error("error: field: {}, default message:{}, agruments:{}, rejected value{}", m.getField(), m.getDefaultMessage(),
-//                        m.getArguments(), m.getRejectedValue()));
-//        return e.getMessage();
-//    }
-
-//    @ExceptionHandler(ResourceNotFoundException.class)
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    public String resourceNotFoundHandler(ResourceNotFoundException e) {
-//        log.error(String.format("ResourceNotFoundException: %s", e.getMessage()));
-//        return e.getMessage();
-//    }
-
-//    @ExceptionHandler(value = {AppRuntimeException.class})
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    public ResponseEntity<Object> hendleApiException(AppRuntimeException ex) {
-//
-//        //1. paylod exception diteil
-//        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
-//        ApiException apiException = new ApiException(ex.getMessage(), ex,
-//                badRequest, ZonedDateTime.now(ZoneId.of("Z")));
-//        //2. return response entity
-//        return new ResponseEntity<>(apiException, badRequest);
-//    }
-
-
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    public Map<String, String> handleMethodArgumentNotValidExceptionMap(MethodArgumentNotValidException ex) {
-//
-//        HashMap<String, String> errorMap = new HashMap<>();
-//
-//        ex.getBindingResult().getFieldErrors().forEach(error -> {
-//            errorMap.put(error.getField(), error.getDefaultMessage());
-//        });
-//        return errorMap;
-//    }
-
-
-    //    private HttpStatus mapExceptionMessageCodeToHttpStatus(String status) {
-//        return switch (status) {
-//            case ExceptionMessage.I001, ExceptionMessage.W001 -> HttpStatus.NOT_FOUND;
-//            default -> HttpStatus.OK;
-//        };
-//    }
-    private HttpStatus mapExceptionMessageCodeToHttpStatus(int code) {
-        return switch (code) {
-            case ExceptionMessage.CODE_BAD_REQUEST -> HttpStatus.BAD_REQUEST;
-            case ExceptionMessage.CODE_NOT_FOUND -> HttpStatus.NOT_FOUND;
-            case ExceptionMessage.CODE_SERVER_ERROR -> HttpStatus.INTERNAL_SERVER_ERROR;
-            default -> HttpStatus.OK;
-        };
-    }
-
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public String handleConstraintViolationException(ConstraintViolationException e) {
@@ -149,5 +79,75 @@ class GlobalExceptionHandlerController extends ResponseEntityExceptionHandler {
     public String resourceNotDeletedHandler(ResourceNotDeletedException e) {
         log.error(String.format("ResourceNotDeletedException: %s", e.getMessage()));
         return e.getMessage();
+    }
+    //    @ExceptionHandler(Throwable.class)
+//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//    public String catchThrowableException(Throwable e) {
+//        log.error("Throwable", e);
+//        return e.getMessage();
+
+//    }
+//    @ExceptionHandler(AppRuntimeException.class)
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    public String catchAppRuntimeException(AppRuntimeException e) {
+//        log.error("AppRuntimeException", e);
+//        return e.getMessage();
+
+//    }
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    public String handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+//        e.getBindingResult().getFieldErrors().forEach(m ->
+//                log.error("error: field: {}, default message:{}, agruments:{}, rejected value{}", m.getField(), m.getDefaultMessage(),
+//                        m.getArguments(), m.getRejectedValue()));
+//        return e.getMessage();
+
+//    }
+//    @ExceptionHandler(ResourceNotFoundException.class)
+//    @ResponseStatus(HttpStatus.NOT_FOUND)
+//    public String resourceNotFoundHandler(ResourceNotFoundException e) {
+//        log.error(String.format("ResourceNotFoundException: %s", e.getMessage()));
+//        return e.getMessage();
+
+//    }
+//    @ExceptionHandler(value = {AppRuntimeException.class})
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    public ResponseEntity<Object> hendleApiException(AppRuntimeException ex) {
+//
+//        //1. paylod exception diteil
+//        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+//        ApiException apiException = new ApiException(ex.getMessage(), ex,
+//                badRequest, ZonedDateTime.now(ZoneId.of("Z")));
+//        //2. return response entity
+//        return new ResponseEntity<>(apiException, badRequest);
+
+//    }
+
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public Map<String, String> handleMethodArgumentNotValidExceptionMap(MethodArgumentNotValidException ex) {
+//
+//        HashMap<String, String> errorMap = new HashMap<>();
+//
+//        ex.getBindingResult().getFieldErrors().forEach(error -> {
+//            errorMap.put(error.getField(), error.getDefaultMessage());
+//        });
+//        return errorMap;
+
+//    }
+
+    //    private HttpStatus mapExceptionMessageCodeToHttpStatus(String status) {
+//        return switch (status) {
+//            case ExceptionMessage.I001, ExceptionMessage.W001 -> HttpStatus.NOT_FOUND;
+//            default -> HttpStatus.OK;
+//        };
+//    }
+
+    private HttpStatus mapExceptionMessageCodeToHttpStatus(int code) {
+        return switch (code) {
+            case ExceptionMessage.CODE_BAD_REQUEST -> HttpStatus.BAD_REQUEST;
+            case ExceptionMessage.CODE_NOT_FOUND -> HttpStatus.NOT_FOUND;
+            case ExceptionMessage.CODE_SERVER_ERROR -> HttpStatus.INTERNAL_SERVER_ERROR;
+            default -> HttpStatus.OK;
+        };
     }
 }
--- src/main/java/pl/byczazagroda/trackexpensesappbackend/exception/ResourceNotDeletedException.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/exception/ResourceNotDeletedException.java
@@ -1,6 +1,6 @@
 package pl.byczazagroda.trackexpensesappbackend.exception;
 
-public class ResourceNotDeletedException extends AppRuntimeException{
+public class ResourceNotDeletedException extends RuntimeException{
 
     public ResourceNotDeletedException(String message){
         super(message);
