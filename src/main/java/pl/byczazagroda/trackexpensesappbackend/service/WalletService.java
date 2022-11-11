2b3a4e5bce86b92fc50fa66525b60e307e245637
Krisu <krstr93@gmail.com>
2022-11-11 14:14:10 +0100

    corrections after code review

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/service/WalletService.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/service/WalletService.java
@@ -12,9 +12,9 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Validated
