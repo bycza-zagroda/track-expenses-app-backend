8738bd8aa0d6af000e4b783a02dba83bb7b755fa
Piotr Mackowiak <mackowiak55@gmail.com>
2023-01-08 19:02:39 +0100

    feat: Add endpoint in FinancialTransactionController

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/controller/FinancialTransactionController.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/controller/FinancialTransactionController.java
@@ -1,13 +1,30 @@
 package pl.byczazagroda.trackexpensesappbackend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.byczazagroda.trackexpensesappbackend.dto.FinancialTransactionDTO;
 import pl.byczazagroda.trackexpensesappbackend.service.FinancialTransactionService;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("api/transactions/")
 public class FinancialTransactionController {
 
     private final FinancialTransactionService financialTransactionService;
 
+    @GetMapping("/{id}")
+    public ResponseEntity<FinancialTransactionDTO> findTransactionById(@Min(1) @NotNull @PathVariable Long id) {
+
+        FinancialTransactionDTO financialTransaction = financialTransactionService.findById(id);
+        return new ResponseEntity<>(financialTransaction, HttpStatus.OK);
+
+    }
 }
