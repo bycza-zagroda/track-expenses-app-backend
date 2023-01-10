96d8a57b43562876c8eaa591c2992a0d5075bcf3
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-01-10 13:49:21 +0100

    feat: Add getFinancialTransactionsByWalletId

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/service/FinancialTransactionServiceImpl.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/service/FinancialTransactionServiceImpl.java
@@ -3,7 +3,11 @@ package pl.byczazagroda.trackexpensesappbackend.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.byczazagroda.trackexpensesappbackend.dto.FinancialTransactionDTO;
+import pl.byczazagroda.trackexpensesappbackend.mapper.FinancialTransactionModelMapper;
 import pl.byczazagroda.trackexpensesappbackend.repository.FinancialTransactionRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +15,12 @@ public class FinancialTransactionServiceImpl implements FinancialTransactionServ
 
     private final FinancialTransactionRepository financialTransactionRepository;
 
+    private final FinancialTransactionModelMapper financialTransactionModelMapper;
+
+    @Override
+    public List<FinancialTransactionDTO> getFinancialTransactionsByWalletId(Long walletId) {
+        return financialTransactionRepository.findAllByWalletIdOrderByTransactionDateDesc(walletId).stream()
+                .map(financialTransactionModelMapper::mapFinancialTransactionEntityToFinancialTransactionDTO)
+                .toList();
+    }
 }
