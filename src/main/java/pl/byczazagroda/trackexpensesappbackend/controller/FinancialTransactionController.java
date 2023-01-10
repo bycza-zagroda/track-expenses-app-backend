8fb71bc9286bcf60793d0a6be6f41b1375f82cd4
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-01-10 13:49:50 +0100

    feat: Add getFinancialTransactionsByWalletId

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/controller/FinancialTransactionController.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/controller/FinancialTransactionController.java
@@ -1,13 +1,30 @@
 package pl.byczazagroda.trackexpensesappbackend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pl.byczazagroda.trackexpensesappbackend.dto.FinancialTransactionDTO;
 import pl.byczazagroda.trackexpensesappbackend.service.FinancialTransactionService;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("api/transactions")
 public class FinancialTransactionController {
 
     private final FinancialTransactionService financialTransactionService;
 
+
+    @GetMapping()
+    ResponseEntity<List<FinancialTransactionDTO>> getFinancialTransactionsByWalletId(@RequestParam @Min(1) @NotNull Long walletId) {
+        List<FinancialTransactionDTO> financialTransactionDTOList = financialTransactionService.getFinancialTransactionsByWalletId(walletId);
+        return new ResponseEntity<>(financialTransactionDTOList, HttpStatus.OK);
+    }
 }
