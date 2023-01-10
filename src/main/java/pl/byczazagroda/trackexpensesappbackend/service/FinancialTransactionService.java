72027baa6b070c2572b91d1061de34d885b34712
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-01-10 13:49:11 +0100

    feat: Add getFinancialTransactionsByWalletId

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/service/FinancialTransactionService.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/service/FinancialTransactionService.java
@@ -1,4 +1,10 @@
 package pl.byczazagroda.trackexpensesappbackend.service;
 
+import pl.byczazagroda.trackexpensesappbackend.dto.FinancialTransactionDTO;
+
+import java.util.List;
+
 public interface FinancialTransactionService {
+
+    List<FinancialTransactionDTO> getFinancialTransactionsByWalletId(Long walletId);
 }
