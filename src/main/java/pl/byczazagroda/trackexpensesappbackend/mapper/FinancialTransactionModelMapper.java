694306e9174f094c85f9b6a516983ed791a33ab3
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-01-10 13:48:04 +0100

    feat: Add FinancialTransactionModelMapper

---
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/mapper/FinancialTransactionModelMapper.java
@@ -0,0 +1,10 @@
+package pl.byczazagroda.trackexpensesappbackend.mapper;
+
+import org.mapstruct.Mapper;
+import pl.byczazagroda.trackexpensesappbackend.dto.FinancialTransactionDTO;
+import pl.byczazagroda.trackexpensesappbackend.model.FinancialTransaction;
+
+@Mapper(componentModel = "spring")
+public interface FinancialTransactionModelMapper {
+    FinancialTransactionDTO mapFinancialTransactionEntityToFinancialTransactionDTO(FinancialTransaction transaction);
+}
