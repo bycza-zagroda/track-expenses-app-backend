5217540261ab9e2fe7962544b6faa00d9b310502
RobertoJavaDev <robertojavadev@gmail.com>
2023-01-12 20:13:18 +0100

    feat: Implemented updateTransaction method to service

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/service/FinancialTransactionService.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/service/FinancialTransactionService.java
@@ -1,10 +1,14 @@
 package pl.byczazagroda.trackexpensesappbackend.service;
 
 import pl.byczazagroda.trackexpensesappbackend.dto.FinancialTransactionDTO;
+import pl.byczazagroda.trackexpensesappbackend.dto.UpdateFinancialTransactionDTO;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 public interface FinancialTransactionService {
     FinancialTransactionDTO findById(@Min(1) @NotNull Long id);
+
+    FinancialTransactionDTO updateTransaction(@Min(1) @NotNull Long id, @Valid UpdateFinancialTransactionDTO updateTransactionDTO);
 }
--- src/main/java/pl/byczazagroda/trackexpensesappbackend/service/FinancialTransactionServiceImpl.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/service/FinancialTransactionServiceImpl.java
@@ -3,19 +3,23 @@ package pl.byczazagroda.trackexpensesappbackend.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 import pl.byczazagroda.trackexpensesappbackend.dto.FinancialTransactionDTO;
+import pl.byczazagroda.trackexpensesappbackend.dto.UpdateFinancialTransactionDTO;
 import pl.byczazagroda.trackexpensesappbackend.exception.AppRuntimeException;
 import pl.byczazagroda.trackexpensesappbackend.exception.ErrorCode;
 import pl.byczazagroda.trackexpensesappbackend.mapper.FinancialTransactionModelMapper;
 import pl.byczazagroda.trackexpensesappbackend.model.FinancialTransaction;
 import pl.byczazagroda.trackexpensesappbackend.repository.FinancialTransactionRepository;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Validated
 public class FinancialTransactionServiceImpl implements FinancialTransactionService {
 
     private final FinancialTransactionRepository financialTransactionRepository;
@@ -27,5 +31,23 @@ public class FinancialTransactionServiceImpl implements FinancialTransactionServ
         return financialTransaction.map(financialTransactionModelMapper::mapFinancialTransactionEntityToFinancialTransactionDTO)
                 .orElseThrow(() -> new AppRuntimeException(ErrorCode.FT01,
                         String.format("Financial transaction with id: %d not found", id)));
+    }
+
+    @Override
+    public FinancialTransactionDTO updateTransaction(
+            @Min(1) @NotNull Long id,
+            @Valid UpdateFinancialTransactionDTO updateTransactionDTO){
+
+        FinancialTransaction financialTransaction = financialTransactionRepository.findById(id)
+                .orElseThrow(()-> {
+                    throw new AppRuntimeException(ErrorCode.FT01,
+                            String.format("Financial transaction with id: %d not found", id));
+                });
+
+        financialTransaction.setAmount(updateTransactionDTO.amount());
+        financialTransaction.setDescription(updateTransactionDTO.description());
+        financialTransaction.setTransactionDate(updateTransactionDTO.transactionDate());
+
+        return financialTransactionModelMapper.mapFinancialTransactionEntityToFinancialTransactionDTO(financialTransaction);
     }
 }
