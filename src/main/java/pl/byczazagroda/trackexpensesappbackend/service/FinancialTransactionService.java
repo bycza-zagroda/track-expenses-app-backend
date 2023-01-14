18ebe4106eec25c532501a440c58b18e87bb62a1
Piotr Mackowiak <mackowiak55@gmail.com>
2023-01-14 15:21:58 +0100

    feat: Add FinancialTransactionService

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/service/FinancialTransactionService.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/service/FinancialTransactionService.java
@@ -1,10 +1,13 @@
 package pl.byczazagroda.trackexpensesappbackend.service;
 
+import pl.byczazagroda.trackexpensesappbackend.dto.CreateFinancialTransactionDTO;
 import pl.byczazagroda.trackexpensesappbackend.dto.FinancialTransactionDTO;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 public interface FinancialTransactionService {
+    FinancialTransactionDTO createFinancialTransaction(@Valid CreateFinancialTransactionDTO createFinancialTransactionDTO);
     FinancialTransactionDTO findById(@Min(1) @NotNull Long id);
 }
--- src/main/java/pl/byczazagroda/trackexpensesappbackend/service/FinancialTransactionServiceImpl.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/service/FinancialTransactionServiceImpl.java
@@ -3,15 +3,20 @@ package pl.byczazagroda.trackexpensesappbackend.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.byczazagroda.trackexpensesappbackend.dto.CreateFinancialTransactionDTO;
 import pl.byczazagroda.trackexpensesappbackend.dto.FinancialTransactionDTO;
 import pl.byczazagroda.trackexpensesappbackend.exception.AppRuntimeException;
 import pl.byczazagroda.trackexpensesappbackend.exception.ErrorCode;
 import pl.byczazagroda.trackexpensesappbackend.mapper.FinancialTransactionModelMapper;
 import pl.byczazagroda.trackexpensesappbackend.model.FinancialTransaction;
+import pl.byczazagroda.trackexpensesappbackend.model.Wallet;
 import pl.byczazagroda.trackexpensesappbackend.repository.FinancialTransactionRepository;
+import pl.byczazagroda.trackexpensesappbackend.repository.WalletRepository;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import java.time.Instant;
 import java.util.Optional;
 
 @Service
@@ -20,6 +25,26 @@ public class FinancialTransactionServiceImpl implements FinancialTransactionServ
 
     private final FinancialTransactionRepository financialTransactionRepository;
     private final FinancialTransactionModelMapper financialTransactionModelMapper;
+    private final WalletRepository walletRepository;
+
+    @Override
+    public FinancialTransactionDTO createFinancialTransaction(@Valid CreateFinancialTransactionDTO createFinancialTransactionDTO) {
+        Long walletId = createFinancialTransactionDTO.walletId();
+        Wallet wallet = walletRepository.findById(walletId).orElseThrow(() -> {
+            throw new AppRuntimeException(
+                    ErrorCode.W003,
+                    String.format("Wallet with id: %d does not exist", walletId));
+        });
+        FinancialTransaction financialTransaction = new FinancialTransaction();
+        financialTransaction.setTransactionDate(Instant.now());
+        financialTransaction.setAmount(createFinancialTransactionDTO.amount());
+        financialTransaction.setWallet(wallet);
+        financialTransaction.setFinancialTransactionType(createFinancialTransactionDTO.financialTransactionType());
+        financialTransaction.setDescription(createFinancialTransactionDTO.description());
+
+        FinancialTransaction savedFinancialTransaction = financialTransactionRepository.save(financialTransaction);
+        return financialTransactionModelMapper.mapFinancialTransactionEntityToFinancialTransactionDTO(savedFinancialTransaction);
+    }
 
     @Override
     public FinancialTransactionDTO findById(@Min(1) @NotNull Long id) {
