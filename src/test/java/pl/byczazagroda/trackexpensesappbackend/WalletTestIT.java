8dac03351a29ac62c8350929e63d52c3464fe432
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-01-16 13:08:46 +0100

    test: Add test for creating wallet

--- src/test/java/pl/byczazagroda/trackexpensesappbackend/WalletTestIT.java
+++ src/test/java/pl/byczazagroda/trackexpensesappbackend/WalletTestIT.java
@@ -1,4 +1,32 @@
 package pl.byczazagroda.trackexpensesappbackend;
 
-public class WalletTestIT {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import pl.byczazagroda.trackexpensesappbackend.dto.CreateWalletDTO;
+import pl.byczazagroda.trackexpensesappbackend.repository.WalletRepository;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class WalletTestIT extends BaseControllerTestIT {
+
+    @Autowired
+    private WalletRepository walletRepository;
+
+    @Test
+    @DisplayName("It should create wallet")
+    void shouldCreateWallet() throws Exception {
+
+        CreateWalletDTO createWalletDTO = new CreateWalletDTO("Test name");
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/wallets")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(createWalletDTO)))
+                .andExpect(status().isCreated());
+
+        Assertions.assertEquals(1, walletRepository.findAll().size());
+    }
 }
