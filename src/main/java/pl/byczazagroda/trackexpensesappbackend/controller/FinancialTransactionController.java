4fba0db7ddfec63c0c5e08e75150fad2df0c08d0
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-01-16 13:14:06 +0100

    refactor: Replace wildcards with normal imports

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/controller/FinancialTransactionController.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/controller/FinancialTransactionController.java
@@ -3,7 +3,11 @@ package pl.byczazagroda.trackexpensesappbackend.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import pl.byczazagroda.trackexpensesappbackend.dto.FinancialTransactionDTO;
 import pl.byczazagroda.trackexpensesappbackend.service.FinancialTransactionService;
 
@@ -24,6 +28,7 @@ public class FinancialTransactionController {
         List<FinancialTransactionDTO> financialTransactionDTOList = financialTransactionService.getFinancialTransactionsByWalletId(walletId);
         return new ResponseEntity<>(financialTransactionDTOList, HttpStatus.OK);
     }
+
     @GetMapping("/{id}")
     public ResponseEntity<FinancialTransactionDTO> findTransactionById(@Min(1) @NotNull @PathVariable Long id) {
 
