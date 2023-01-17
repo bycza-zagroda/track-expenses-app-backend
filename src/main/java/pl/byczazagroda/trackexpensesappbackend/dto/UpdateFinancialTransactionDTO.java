5f3e031263f670a4935e1c6fd4e2bb6829be8f12
RobertoJavaDev <robertojavadev@gmail.com>
2023-01-17 18:39:54 +0100

    refactor: Changed validation in Dto class

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/dto/UpdateFinancialTransactionDTO.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/dto/UpdateFinancialTransactionDTO.java
@@ -3,13 +3,13 @@ package pl.byczazagroda.trackexpensesappbackend.dto;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.Digits;
-import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.time.Instant;
 
 public record UpdateFinancialTransactionDTO(
-        @Positive @Digits(integer = 15, fraction = 2) BigDecimal amount,
+        @PositiveOrZero @Digits(integer = 13, fraction = 2) BigDecimal amount,
         @DateTimeFormat(pattern = "yyyy-mm-dd hh:mm:ss") Instant transactionDate,
         @Size(max = 255) String description) {
 }
