8ef3353675d1610ca745d672ca015623b5758523
Piotr Mackowiak <mackowiak55@gmail.com>
2023-01-18 20:16:30 +0100

    refactor: changed annotation in CreateFinancialTransactionDTO

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/dto/CreateFinancialTransactionDTO.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/dto/CreateFinancialTransactionDTO.java
@@ -2,11 +2,12 @@ package pl.byczazagroda.trackexpensesappbackend.dto;
 
 import pl.byczazagroda.trackexpensesappbackend.model.FinancialTransactionType;
 
-import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 
-public record CreateFinancialTransactionDTO(@Min(1) @NotNull Long walletId, @DecimalMin("0.0") BigDecimal amount,
+public record CreateFinancialTransactionDTO(@Min(1) @NotNull Long walletId, @Digits(integer = 13, fraction = 2) @PositiveOrZero BigDecimal amount,
                                             String description, FinancialTransactionType financialTransactionType) {
 }
