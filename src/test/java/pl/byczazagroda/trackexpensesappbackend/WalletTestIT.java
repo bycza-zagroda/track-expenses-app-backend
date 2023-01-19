7d11dd1509a588882d22854884a7862eeccabd7d
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-01-19 15:00:49 +0100

    refactor: Make database clean after each test

--- src/test/java/pl/byczazagroda/trackexpensesappbackend/WalletTestIT.java
+++ src/test/java/pl/byczazagroda/trackexpensesappbackend/WalletTestIT.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import pl.byczazagroda.trackexpensesappbackend.dto.CreateWalletDTO;
 import pl.byczazagroda.trackexpensesappbackend.repository.WalletRepository;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class WalletTestIT extends BaseControllerTestIT {
     @Autowired
     private WalletRepository walletRepository;
