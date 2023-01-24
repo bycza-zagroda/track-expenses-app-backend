c40378fe5c7feeb18d7206c0ca5e3853b5b06fdb
RobertoJavaDev <robertojavadev@gmail.com>
2023-01-24 09:15:08 +0100

    feat: Added validation on financial transaction type

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/dto/UpdateFinancialTransactionDTO.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/dto/UpdateFinancialTransactionDTO.java
@@ -1,8 +1,10 @@
 package pl.byczazagroda.trackexpensesappbackend.dto;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import pl.byczazagroda.trackexpensesappbackend.model.FinancialTransactionType;
 
 import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 import javax.validation.constraints.Size;
 import java.math.BigDecimal;
@@ -11,5 +13,6 @@ import java.time.Instant;
 public record UpdateFinancialTransactionDTO(
         @PositiveOrZero @Digits(integer = 13, fraction = 2) BigDecimal amount,
         @DateTimeFormat(pattern = "yyyy-mm-dd hh:mm:ss") Instant transactionDate,
-        @Size(max = 255) String description) {
+        @Size(max = 255) String description,
+        @NotNull FinancialTransactionType financialTransactionType) {
 }
