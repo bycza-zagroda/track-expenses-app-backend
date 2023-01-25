692d22d38d64abc6da3ca0779de77d789f320e39
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-01-25 18:43:14 +0100

    refactor: Add before each method

--- src/test/java/pl/byczazagroda/trackexpensesappbackend/WalletTestIT.java
+++ src/test/java/pl/byczazagroda/trackexpensesappbackend/WalletTestIT.java
@@ -1,21 +1,25 @@
 package pl.byczazagroda.trackexpensesappbackend;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import pl.byczazagroda.trackexpensesappbackend.dto.CreateWalletDTO;
 import pl.byczazagroda.trackexpensesappbackend.repository.WalletRepository;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class WalletTestIT extends BaseControllerTestIT {
     @Autowired
     private WalletRepository walletRepository;
+
+    @BeforeEach
+    public void cleanTable() {
+        walletRepository.deleteAll();
+    }
 
     @Test
     @DisplayName("It should create wallet")
