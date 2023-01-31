badc998885897988487a5ebb60841fb5e14e2141
Piotr Mackowiak <mackowiak55@gmail.com>
2023-01-31 09:01:41 +0100

    refactor: Refactor method findById, checking for exceptions before mapping

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/service/FinancialTransactionServiceImpl.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/service/FinancialTransactionServiceImpl.java
@@ -21,7 +21,7 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.time.Instant;
-import java.util.Optional;
+
 
 @Service
 @RequiredArgsConstructor
@@ -61,10 +61,11 @@ public class FinancialTransactionServiceImpl implements FinancialTransactionServ
 
     @Override
     public FinancialTransactionDTO findById(@Min(1) @NotNull Long id) {
-        Optional<FinancialTransaction> financialTransaction = financialTransactionRepository.findById(id);
-        return financialTransaction.map(financialTransactionModelMapper::mapFinancialTransactionEntityToFinancialTransactionDTO)
+        FinancialTransaction financialTransaction = financialTransactionRepository.findById(id)
                 .orElseThrow(() -> new AppRuntimeException(ErrorCode.FT001,
-                        String.format("Financial transaction with id: %d not found", id)));
+                String.format("Financial transaction with id: %d not found", id)));
+        return financialTransactionModelMapper.mapFinancialTransactionEntityToFinancialTransactionDTO(financialTransaction);
+
     }
 
     @Override
