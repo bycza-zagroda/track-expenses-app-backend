16d663c0e163ad039cb32b525046f8b5c6c23893
pohde <22906225+odzio33@users.noreply.github.com>
2023-03-14 23:19:59 +0100

    Delete FinancialTransactionCategoryUpdateDTO.java
    The implementation of this validation does not affect issues related to this branch.

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/dto/FinancialTransactionCategoryUpdateDTO.java
+++
@@ -1,9 +0,0 @@
-package pl.byczazagroda.trackexpensesappbackend.dto;
-
-import pl.byczazagroda.trackexpensesappbackend.model.FinancialTransactionType;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
-public record FinancialTransactionCategoryUpdateDTO(@NotBlank @Size(max = 30, message = "{validation.name.size.too_long}") String name, @NotNull FinancialTransactionType type){}
