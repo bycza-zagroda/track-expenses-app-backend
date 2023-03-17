441646e6c8c281f832d4908c5e67ecba5a365c69
Dawid Bielecki dawciobiel <dawciobiel@gmail.com>
2023-03-17 16:49:19 +0100

    add: size annotation for description in FinancialTransactionCreateDTO

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/dto/FinancialTransactionCreateDTO.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/dto/FinancialTransactionCreateDTO.java
@@ -6,11 +6,12 @@ import javax.validation.constraints.Digits;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.time.Instant;
 
 public record FinancialTransactionCreateDTO(@Min(1) @NotNull Long walletId,
                                             @Digits(integer = 13, fraction = 2) @PositiveOrZero BigDecimal amount,
-                                            String description, Instant date,
+                                            @Size(max = 255) String description, Instant date,
                                             @NotNull FinancialTransactionType type) {
 }
