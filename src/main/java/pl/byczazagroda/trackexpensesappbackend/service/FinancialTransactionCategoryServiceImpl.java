45dbe9e1f16a93cdd5dfa039f85871146e476881
Remigiusz Królak <krolak.remek@gmail.com>
2023-03-27 16:13:02 +0200

    refactor: improve code styling

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/service/FinancialTransactionCategoryServiceImpl.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/service/FinancialTransactionCategoryServiceImpl.java
@@ -47,7 +47,7 @@ public class FinancialTransactionCategoryServiceImpl implements FinancialTransac
 
     @Override
     public void deleteFinancialTransactionCategory(@Min(1) @NotNull Long id) {
-        if(financialTransactionCategoryRepository.existsById(id)) {
+        if (financialTransactionCategoryRepository.existsById(id)) {
             financialTransactionCategoryRepository.deleteById(id);
         } else {
             throw new AppRuntimeException(
