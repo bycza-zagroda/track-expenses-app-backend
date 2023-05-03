27a3cc4916b0877d5dd1621635300f09fa293ae5
McSymilian <77546701+McSymilian@users.noreply.github.com>
2023-05-03 11:45:17 +0200

    integration test for getting all Wallets (#198)
    * test class of all wallets rest query
    
    * test class of all wallets rest query
    
    * Add implemented test method shouldResponseStatusOKAndWalletDTOsList() to check get all wallets
    
    * Get All Wallets Tests with checking content of response
    
    * response print deleted and expected response size check
    
    ---------
    
    Co-authored-by: Iwona <51120038+Iwona007@users.noreply.github.com>

---
+++ src/test/java/pl/byczazagroda/trackexpensesappbackend/integration/GetAllWalletsIT.java
@@ -0,0 +1,79 @@
+package pl.byczazagroda.trackexpensesappbackend.integration;
+
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.ResultActions;
+import pl.byczazagroda.trackexpensesappbackend.BaseIntegrationTestIT;
+import pl.byczazagroda.trackexpensesappbackend.model.Wallet;
+import pl.byczazagroda.trackexpensesappbackend.repository.WalletRepository;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+class GetAllWalletsIT extends BaseIntegrationTestIT {
+
+    @Autowired
+    private WalletRepository walletRepository;
+
+    @BeforeEach
+    public void clearTestDB() {
+        walletRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("when getting all wallets should return wallets DTOs list and response status OK")
+    void shouldResponseStatusOKAndWalletDTOsList() throws Exception {
+        //given
+        List<Wallet> savedWallets = getWalletList();
+
+        walletRepository.saveAll(savedWallets);
+
+        // when
+        ResultActions response = mockMvc.perform(get("/api/wallets"));
+
+        // then
+        response.andExpect(status().isOk())
+                .andExpect(jsonPath("$.*", hasSize(savedWallets.size())))
+                .andExpect(jsonPath("$.[0].name").value(savedWallets.get(0).getName()))
+                .andExpect(jsonPath("$.[0].id").value(savedWallets.get(0).getId()))
+                .andExpect(jsonPath("$.[1].name").value(savedWallets.get(1).getName()))
+                .andExpect(jsonPath("$.[1].id").value(savedWallets.get(1).getId()))
+                .andExpect(jsonPath("$.[2].name").value(savedWallets.get(2).getName()))
+                .andExpect(jsonPath("$.[2].id").value(savedWallets.get(2).getId()))
+                .andExpect(jsonPath("$.[3].name").value(savedWallets.get(3).getName()))
+                .andExpect(jsonPath("$.[3].id").value(savedWallets.get(3).getId()));
+    }
+
+    @Test
+    @DisplayName("when getting all wallets should return no wallets DTOs list and response status OK")
+    void shouldResponseStatusOKAndEmptyWalletDTOsList() throws Exception {
+        //given
+
+        // when
+        ResultActions response = mockMvc.perform(get("/api/wallets"));
+
+        // then
+        response.andExpect(status().isOk())
+                .andExpect(jsonPath("$.*", hasSize(0)));
+
+    }
+
+    @NotNull
+    private static List<Wallet> getWalletList() {
+        return List.of(
+                new Wallet("TestWallet0"),
+                new Wallet("TestWallet1"),
+                new Wallet("TestWallet2"),
+                new Wallet("TestWallet3")
+        );
+    }
+}
