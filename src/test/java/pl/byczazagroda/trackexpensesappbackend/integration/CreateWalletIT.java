c55e80f1b02888c95c1aae1269b5d909d4526e36
Mirosław Gabruś <github@mirekgab.pl>
2023-05-19 08:39:09 +0200

    I renamed the integration test classes. (#246)
    Co-authored-by: Miroslaw Gabrus <poczta@mirekgab.pl>

--- src/test/java/pl/byczazagroda/trackexpensesappbackend/integration/CreateWalletIT.java
+++ src/test/java/pl/byczazagroda/trackexpensesappbackend/integration/CreateWalletIT.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-class TestCreateWallet extends BaseIntegrationTestIT {
+class CreateWalletIT extends BaseIntegrationTestIT {
 
     @Autowired
     private WalletRepository walletRepository;
