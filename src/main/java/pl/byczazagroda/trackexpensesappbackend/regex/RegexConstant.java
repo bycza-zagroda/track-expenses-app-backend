3006e8fd79c295a43007e6c496d5f4156c80abc3
Iwona007 <iwonka.adamkiewicz@gmail.com>
2023-07-06 21:00:34 +0200

    coder review add more test cases to check token and extend regex

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/regex/RegexConstant.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/regex/RegexConstant.java
@@ -3,6 +3,6 @@ package pl.byczazagroda.trackexpensesappbackend.regex;
 public final class RegexConstant {
     private RegexConstant() { }
 
-    public static final String PASSWORD_PATTERN = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[@$!%?&]).{8,100}$";
+    public static final String PASSWORD_PATTERN = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%^&*?])[A-Za-z\\d!@#$%^&*?]{8,100}$";
     public static final String EMAIL_PATTERN = "^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
 }
