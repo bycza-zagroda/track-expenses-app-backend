dc3faee5ee3db42d7a43eb30560dd09884d138e5
Sebastian Glizia <109149500+Dharakkkk@users.noreply.github.com>
2023-07-25 23:49:15 +0200

    Implement Refresh Token functionality and tests (#279)
    * Implement Refresh Token functionality and tests
    
    This commit includes the implementation of refresh tokens functionality in the auth controller, as well as associated tests. Here are the detailed notes on the changes made:
    
    Implementation of the POST /api/auth/refresh endpoint in the auth controller. This function retrieves the refresh token from the request, validates it, and creates a new access token and refresh token. The new refresh token is then added to the response as a cookie.
    
    Implementation of a series of unit tests for the aforementioned function. These tests cover different scenarios such as the case when both the access token and the refresh token are valid, absence of the refresh token, incorrect access token, incorrect refresh token, and the case when the access token has expired.
    
    Moving the RefreshTokenTest tests to the 'controller' package, since these tests are focused on the behavior of the auth controller.
    
    Updating the names of the tests to be more descriptive and adding @DisplayName annotations to each test to make it easier to understand what each test does.
    
    This commit aims to provide refresh tokens functionality, which is essential for the security and convenience of users.
    
    * Code formatting
    
    Code Formatting
    
    * Implement token refreshing 2.0
    
    - removed logic from controller
    - access token is now validated
    - refreshTokenTest is now refreshTokenIT
    - refreshTokenIT is in the integration package
    - refreshToken method is better now
    
    * Code formatting
    
    - code is preetier now
    
    * Code formatting
    
    - new error code
    - code formatting

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/config/WebSecurityConfig.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/config/WebSecurityConfig.java
@@ -33,6 +33,7 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .antMatchers("/").permitAll()
                         .antMatchers("/api/auth/login").permitAll()
+                        .antMatchers("/api/auth/refresh").permitAll()
                         .antMatchers("/api/users/me", "/api/categories/**").authenticated()
                         .anyRequest().permitAll());
         http
--- src/main/java/pl/byczazagroda/trackexpensesappbackend/controller/AuthController.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/controller/AuthController.java
@@ -1,19 +1,17 @@
 package pl.byczazagroda.trackexpensesappbackend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.byczazagroda.trackexpensesappbackend.dto.AuthAccessTokenDTO;
 import pl.byczazagroda.trackexpensesappbackend.dto.AuthLoginDTO;
 import pl.byczazagroda.trackexpensesappbackend.dto.AuthRegisterDTO;
 import pl.byczazagroda.trackexpensesappbackend.service.UserService;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
@@ -23,7 +21,8 @@ import javax.validation.Valid;
 @Validated
 public class AuthController {
 
-    private final UserService userService;
+    @Autowired
+    private UserService userService;
 
     @PostMapping("/login")
     public ResponseEntity<AuthAccessTokenDTO> authenticateUser(@Valid @RequestBody AuthLoginDTO authLoginDTO,
@@ -45,8 +44,15 @@ public class AuthController {
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<AuthAccessTokenDTO> refreshToken(@CookieValue(name = "refresh_token") String refreshToken) {
-        //TODO
-        return new ResponseEntity<>(new AuthAccessTokenDTO("TODO-accessTOKEN"), HttpStatus.OK);
+    public ResponseEntity<AuthAccessTokenDTO> refreshToken(
+            @CookieValue(name = "refresh_token") String refreshToken,
+            @RequestHeader(name = "Authorization") String accessToken,
+            HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+            String newAccessToken = userService.refreshToken(request, response, refreshToken, accessToken);
+            AuthAccessTokenDTO accessTokenDTO = new AuthAccessTokenDTO(newAccessToken);
+
+            return ResponseEntity.ok(accessTokenDTO);
     }
+
 }
