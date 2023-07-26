4a86fa8d44522d60415532b6391fbc4631059804
sylvvester <128241313+sylvvester@users.noreply.github.com>
2023-07-26 12:45:34 +0200

    Create an integration tests for create category endpoint. #262

--- src/test/java/pl/byczazagroda/trackexpensesappbackend/integration/CreateFinancialTransactionCategoryIT.java
+++ src/test/java/pl/byczazagroda/trackexpensesappbackend/integration/CreateFinancialTransactionCategoryIT.java
@@ -6,14 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import pl.byczazagroda.trackexpensesappbackend.BaseIntegrationTestIT;
 import pl.byczazagroda.trackexpensesappbackend.dto.FinancialTransactionCategoryCreateDTO;
-import pl.byczazagroda.trackexpensesappbackend.dto.FinancialTransactionCategoryDTO;
 import pl.byczazagroda.trackexpensesappbackend.repository.FinancialTransactionCategoryRepository;
 import pl.byczazagroda.trackexpensesappbackend.repository.FinancialTransactionRepository;
 import pl.byczazagroda.trackexpensesappbackend.repository.UserRepository;
 import pl.byczazagroda.trackexpensesappbackend.repository.WalletRepository;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import pl.byczazagroda.trackexpensesappbackend.exception.ErrorCode;
-import pl.byczazagroda.trackexpensesappbackend.model.FinancialTransactionCategory;
 import pl.byczazagroda.trackexpensesappbackend.model.FinancialTransactionType;
 import pl.byczazagroda.trackexpensesappbackend.model.User;
 import pl.byczazagroda.trackexpensesappbackend.model.UserStatus;
@@ -44,64 +42,39 @@ class CreateFinancialTransactionCategoryIT extends BaseIntegrationTestIT {
 
     @BeforeEach
     void clearDatabase() {
-//        financialTransactionRepository.deleteAll();
-//        financialTransactionCategoryRepository.deleteAll();
-//        walletRepository.deleteAll();
-//        userRepository.deleteAll();
-
-        User user = new User();
-        user.setId(1L);
-        user.setEmail("test@email.com");
-        user.setPassword("password");
-        user.setUserName("username");
-        user.setUserStatus(UserStatus.VERIFIED);
-        userRepository.save(user);
+        financialTransactionRepository.deleteAll();
+        financialTransactionCategoryRepository.deleteAll();
+        walletRepository.deleteAll();
+        userRepository.deleteAll();
     }
 
-    /*
-    @PostMapping()
-    public ResponseEntity<FinancialTransactionCategoryDTO> createFinancialTransactionCategory(
-            @RequestBody FinancialTransactionCategoryCreateDTO financialTransactionCategoryCreateDTO,
-            Principal principal) {
-
-        Long userId = Long.valueOf(principal.getName());
-
-        FinancialTransactionCategoryDTO financialTransactionCategoryDTO =
-                financialTransactionCategoryService.createFinancialTransactionCategory(financialTransactionCategoryCreateDTO, userId);
-
-        return new ResponseEntity<>(financialTransactionCategoryDTO, HttpStatus.CREATED);
-    }
-    */
 
     @DisplayName("Should successfully create financial transaction category")
     @Test
-    void testCreate() throws Exception {
-        var financialTransactionCategoryCreateDTO = new FinancialTransactionCategoryCreateDTO
+    void testCreateFinancialTransactionCategory_whenValidDataProvided_thenShouldCreateCategorySuccessfully() throws Exception {
+        User testUser = createTestUser();
+        FinancialTransactionCategoryCreateDTO financialTransactionCategoryCreateDTO = new FinancialTransactionCategoryCreateDTO
                 ("Category",
-                FinancialTransactionType.INCOME,
-                        1L);
-
+                        FinancialTransactionType.INCOME,
+                        testUser.getId());
 
         mockMvc.perform(post("/api/categories")
-                        .contentType(APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(financialTransactionCategoryCreateDTO))
-                        .with(user("1")))
-//                        .principal(principal)
-//                        .with(csrf()))
+                .contentType(APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(financialTransactionCategoryCreateDTO))
+                .with(user(String.valueOf(testUser.getId())))
+                        .with(csrf()))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.name").value("Category"))
                 .andExpect(jsonPath("$.type").value("INCOME"));
 
         assertEquals(1, financialTransactionCategoryRepository.count());
-        assertEquals(0, walletRepository.count());
-        assertEquals(1, userRepository.count());
 
 
     }
 
     @DisplayName("Should return error when name length is greater than 30")
     @Test
-    void testCreateNameLengthGreaterThan30() throws Exception {
+    void testCreateFinancialTransactionCategory_whenNameExceeds30Characters_thenShouldReturnError () throws Exception {
         var financialTransactionCategoryCreateDTO = new FinancialTransactionCategoryCreateDTO
                 ("ThisIsVeryLongNameForCategoryMoreThan30Characters",
                         FinancialTransactionType.INCOME,
@@ -117,14 +90,11 @@ class CreateFinancialTransactionCategoryIT extends BaseIntegrationTestIT {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.statusCode").value(ErrorCode.TEA003.getBusinessStatusCode()));
 
         assertEquals(0, financialTransactionCategoryRepository.count());
-        assertEquals(0, walletRepository.count());
-        assertEquals(1, userRepository.count());
     }
-
 
     @DisplayName("Should return error when name is empty")
     @Test
-    void testCreateNameEmpty() throws Exception {
+    void testCreateFinancialTransactionCategory_whenNameIsEmpty_thenShouldReturnError() throws Exception{
         var financialTransactionCategoryCreateDTO = new FinancialTransactionCategoryCreateDTO
                 ("",
                         FinancialTransactionType.INCOME,
@@ -141,13 +111,12 @@ class CreateFinancialTransactionCategoryIT extends BaseIntegrationTestIT {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.statusCode").value(ErrorCode.TEA003.getBusinessStatusCode()));
 
         assertEquals(0, financialTransactionCategoryRepository.count());
-        assertEquals(0, walletRepository.count());
-        assertEquals(1, userRepository.count());
     }
 
     @DisplayName("Should return error when name contains invalid characters")
     @Test
-    void testCreateNameContainsInvalidCharacters() throws Exception {
+    void testCreateFinancialTransactionCategory_whenNameContainsInvalidCharacters_thenShouldReturnError() throws Exception {
+        User testUser = createTestUser();
         var financialTransactionCategoryCreateDTO = new FinancialTransactionCategoryCreateDTO
                 ("Catego*&*^ry@",
                         FinancialTransactionType.INCOME,
@@ -156,41 +125,13 @@ class CreateFinancialTransactionCategoryIT extends BaseIntegrationTestIT {
         mockMvc.perform(post("/api/categories")
                         .contentType(APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(financialTransactionCategoryCreateDTO))
-                        .with(user("1"))
-                        .with(csrf()))
+                        .with(user(String.valueOf(testUser.getId()))))
                 .andExpect(status().isBadRequest())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(ErrorCode.TEA003.getBusinessStatus()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.message").value(ErrorCode.TEA003.getBusinessMessage()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.statusCode").value(ErrorCode.TEA003.getBusinessStatusCode()));
 
         assertEquals(0, financialTransactionCategoryRepository.count());
-        assertEquals(0, walletRepository.count());
-        assertEquals(1, userRepository.count());
-    }
-
-    //invalid type
-
-    @DisplayName("Should return error when type is invalid")
-    @Test
-    void testCreateInvalidType() throws Exception {
-        var financialTransactionCategoryCreateDTO = new FinancialTransactionCategoryCreateDTO
-                ("Category",
-                        FinancialTransactionType.valueOf("INVALID"),
-                        1L);
-
-        mockMvc.perform(post("/api/categories")
-                        .contentType(APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(financialTransactionCategoryCreateDTO))
-                        .with(user("1"))
-                        .with(csrf()))
-                .andExpect(status().isBadRequest())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(ErrorCode.TEA003.getBusinessStatus()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value(ErrorCode.TEA003.getBusinessMessage()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.statusCode").value(ErrorCode.TEA003.getBusinessStatusCode()));
-
-        assertEquals(0, financialTransactionCategoryRepository.count());
-        assertEquals(0, walletRepository.count());
-        assertEquals(1, userRepository.count());
     }
 
     private User createTestUser() {
@@ -204,18 +145,4 @@ class CreateFinancialTransactionCategoryIT extends BaseIntegrationTestIT {
         return userRepository.save(userOne);
     }
 
-
-    private FinancialTransactionCategory createTestFinancialTransactionCategory() {
-        final FinancialTransactionCategory testFinancialTransactionCategory = FinancialTransactionCategory.builder()
-                .name("name")
-                .type(FinancialTransactionType.INCOME)
-                .user(createTestUser())
-                .build();
-        return financialTransactionCategoryRepository.save(testFinancialTransactionCategory);
-    }
-
-        private FinancialTransactionCategoryDTO createTestFinancialTransactionCategoryDTO (){
-        return new FinancialTransactionCategoryDTO(1L, "name", FinancialTransactionType.INCOME, 1L);
-
-    }
 }
