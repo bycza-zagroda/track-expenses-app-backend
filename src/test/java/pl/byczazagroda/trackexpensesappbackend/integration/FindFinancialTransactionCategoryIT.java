75fbb3f70a21d000b1b3fff2ce4cac6608cb88b5
Piotr Mackowiak <mackowiak55@gmail.com>
2023-08-04 14:22:35 +0200

    fix: add clean code changes to FindFinancialTransactionCategoryIT class fix: a different approach to user authorization was used change: add private method to create user instead of adding user in @BeforeEach section

--- src/test/java/pl/byczazagroda/trackexpensesappbackend/integration/FindFinancialTransactionCategoryIT.java
+++ src/test/java/pl/byczazagroda/trackexpensesappbackend/integration/FindFinancialTransactionCategoryIT.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import pl.byczazagroda.trackexpensesappbackend.BaseIntegrationTestIT;
 import pl.byczazagroda.trackexpensesappbackend.model.FinancialTransactionCategory;
 import pl.byczazagroda.trackexpensesappbackend.model.FinancialTransactionType;
@@ -12,69 +14,53 @@ import pl.byczazagroda.trackexpensesappbackend.model.User;
 import pl.byczazagroda.trackexpensesappbackend.model.UserStatus;
 import pl.byczazagroda.trackexpensesappbackend.repository.FinancialTransactionCategoryRepository;
 import pl.byczazagroda.trackexpensesappbackend.repository.UserRepository;
-import pl.byczazagroda.trackexpensesappbackend.service.UserService;
 
 import java.time.Instant;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 public class FindFinancialTransactionCategoryIT extends BaseIntegrationTestIT {
 
     @Autowired
-    FinancialTransactionCategoryRepository financialTransactionCategoryRepository;
+    private FinancialTransactionCategoryRepository financialTransactionCategoryRepository;
 
     @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    private UserService userService;
-
-    private String validAccessToken;
+    private UserRepository userRepository;
 
     @BeforeEach
     public void setup() {
         userRepository.deleteAll();
         financialTransactionCategoryRepository.deleteAll();
-
-        User user = new User();
-        user.setUserName("test");
-        user.setPassword("password");
-        user.setEmail("test@example.com");
-        user.setUserStatus(UserStatus.VERIFIED);
-        userRepository.save(user);
-
-        validAccessToken = userService.createAccessToken(user);
     }
 
     @DisplayName("When search category by id should return proper financial transaction category")
     @Test
     public void testGetFinancialTransactionCategory_whenProperId_shouldReturnFinancialTransactionCategory()
             throws Exception {
-        User userTest = userRepository.findByEmail("test@example.com").get();
-        FinancialTransactionCategory financialTransactionCategory = testFinancialTransactionCategory(userTest);
+        User user = createUser();
+        FinancialTransactionCategory fTCategory = testFinancialTransactionCategory(user);
 
-        mockMvc.perform(get("/api/categories/{id}", financialTransactionCategory.getId())
-                        .header("Authorization", "Bearer " + validAccessToken)
-                        .contentType("application/json"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/categories/{id}", fTCategory.getId())
+                        .with(SecurityMockMvcRequestPostProcessors.user(String.valueOf(user.getId()))))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.financialTransactionCategoryDTO.id")
-                        .value(financialTransactionCategory.getId()))
+                        .value(fTCategory.getId()))
                 .andExpect(jsonPath("$.financialTransactionCategoryDTO.name")
                         .value("testName"));
+
         Assertions.assertEquals(1, financialTransactionCategoryRepository.count());
     }
 
-    @DisplayName("Should return message financial transaction category not found and statusCode: 404")
+    @DisplayName("Should return message financial transaction category not found and HttpStatus Not Found")
     @Test
     public void testGetFinancialTransactionCategory_whenIdIsNotExists_shouldReturnErrorCodeAndStatus404()
             throws Exception {
-        Long nonExistentCategoryId = 999L;
-
-        mockMvc.perform(get("/api/categories/{id}", nonExistentCategoryId)
-                        .header("Authorization", "Bearer " + validAccessToken)
-                        .contentType("application/json"))
+        final Long nonExistentCategoryId = 999L;
+        User user = createUser();
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/categories/{id}", nonExistentCategoryId)
+                .with(SecurityMockMvcRequestPostProcessors.user(String.valueOf(user.getId()))))
                 .andExpect(status().isNotFound())
                 .andReturn();
     }
@@ -82,21 +68,20 @@ public class FindFinancialTransactionCategoryIT extends BaseIntegrationTestIT {
     @DisplayName("Should return list of financial transaction categories")
     @Test
     public void testGetFinancialTransactionCategories() throws Exception {
-        User userTest = userRepository.findByEmail("test@example.com").get();
-        FinancialTransactionCategory financialTransactionCategory = testFinancialTransactionCategory(userTest);
-        FinancialTransactionCategory financialTransactionCategory1 = testFinancialTransactionCategory(userTest);
-        FinancialTransactionCategory financialTransactionCategory2 = testFinancialTransactionCategory(userTest);
-
-        mockMvc.perform(get("/api/categories")
-                        .header("Authorization", "Bearer " + validAccessToken)
-                        .contentType("application/json"))
+        User user = createUser();
+        FinancialTransactionCategory ftc1 = testFinancialTransactionCategory(user);
+        FinancialTransactionCategory ftc2 = testFinancialTransactionCategory(user);
+        FinancialTransactionCategory ftc3 = testFinancialTransactionCategory(user);
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/categories")
+                .with(SecurityMockMvcRequestPostProcessors.user(String.valueOf(user.getId()))))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.[0].id").value(financialTransactionCategory.getId()))
-                .andExpect(jsonPath("$.[1].id").value(financialTransactionCategory1.getId()))
-                .andExpect(jsonPath("$.[2].id").value(financialTransactionCategory2.getId()))
-                .andExpect(jsonPath("$.[0].name").value(financialTransactionCategory.getName()))
-                .andExpect(jsonPath("$.[1].name").value(financialTransactionCategory1.getName()))
-                .andExpect(jsonPath("$.[2].name").value(financialTransactionCategory2.getName()));
+                .andExpect(jsonPath("$.[0].id").value(ftc1.getId()))
+                .andExpect(jsonPath("$.[1].id").value(ftc2.getId()))
+                .andExpect(jsonPath("$.[2].id").value(ftc3.getId()))
+                .andExpect(jsonPath("$.[0].name").value(ftc1.getName()))
+                .andExpect(jsonPath("$.[1].name").value(ftc2.getName()))
+                .andExpect(jsonPath("$.[2].name").value(ftc3.getName()));
+
         Assertions.assertEquals(3, financialTransactionCategoryRepository.count());
     }
 
@@ -108,7 +93,19 @@ public class FindFinancialTransactionCategoryIT extends BaseIntegrationTestIT {
                 .name("testName")
                 .financialTransactions(null)
                 .build();
+
         return financialTransactionCategoryRepository.save(testCategory);
+    }
+
+    private User createUser() {
+        User user = User.builder()
+            .userName("test")
+            .password("password")
+            .email("test@example.com")
+            .userStatus(UserStatus.VERIFIED)
+            .build();
+
+        return userRepository.save(user);
     }
 
 }
