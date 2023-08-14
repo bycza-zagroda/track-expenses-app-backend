394dfbf8dd56ef512de9e27d24a0431d9eb2593a
sylvvester <128241313+sylvvester@users.noreply.github.com>
2023-08-14 08:19:48 +0200

    Create an integration tests for create category endpoint. #262 (#280)
    * Create an integration tests for create category endpoint. #262
    
    * Create an integration tests for create category endpoint. #262
    
    * Refactor code in CreateFinancialTransactionCategoryIT
    
    * added regular expression for category name
    
    * Add integration test for creating financial transaction category when category type is empty
    
    * added @Valid annotation to the createFinancialTransactionCategory method in the  FinancialTransactionCategoryController
    
    * Add assertions in CreateFinancialTransactionCategoryIT tests
    
    * refactor: Remove static imports and reformat code in CreateFinancialTransactionCategoryIT
    
    * refactor: method testCreateFinancialTransactionCategory_whenNameContainsInvalidCharacters_thenShouldReturnBadRequest
    
    * refactor: Use expectAll for validation in CreateFinancialTransactionCategoryIT
    
    - Replaced individual .andExpect statements with .expectAll
    - Updated test validation to handle multiple expectations simultaneously.
    
    * Feature: Add CATEGORY_NAME_PATTERN to FinancialTransactionCategoryCreateDTO for category validation
    
    - Removed unnecessary @NotNull annotation in DTO
    
    * test: Update ErrorCode Value in CreateFinancialTransactionCategoryIT tests
    
    - Modified category name pattern in the test for cases involving invalid characters.
    
    * Update src/test/java/pl/byczazagroda/trackexpensesappbackend/integration/CreateFinancialTransactionCategoryIT.java
    
    Refactor code formatting
    
    * Update src/main/java/pl/byczazagroda/trackexpensesappbackend/regex/RegexConstant.java
    
    -refactor: code formatting
    
    * Update src/test/java/pl/byczazagroda/trackexpensesappbackend/integration/CreateFinancialTransactionCategoryIT.java
    
    code refactoring
    
    * Task create Financial Transaction Category
    
    -refactor: Class was just refactored
    
    * Task create Financial Transaction Category
    
    -refactor: add user id in one of the test
    -refactor: code formatting
    
    * Task create Financial Transaction Category
    
    -refactor: add user id in one of the test
    -refactor: code formatting
    
    * Task create Financial Transaction Category
    
    -fix: fixed test
    
    * Task create Financial Transaction Category
    
    -refactor: code refactoring
    
    * added new regex for the category name
    fix integration test for create category
    
    ---------
    
    Co-authored-by: Miroslaw Gabrus <poczta@mirekgab.pl>
    Co-authored-by: Dawid Bielecki <dawciobiel@gmail.com>
    Co-authored-by: Iwona <51120038+Iwona007@users.noreply.github.com>

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/dto/FinancialTransactionCategoryCreateDTO.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/dto/FinancialTransactionCategoryCreateDTO.java
@@ -1,11 +1,17 @@
 package pl.byczazagroda.trackexpensesappbackend.dto;
 
 import pl.byczazagroda.trackexpensesappbackend.model.FinancialTransactionType;
+import pl.byczazagroda.trackexpensesappbackend.regex.RegexConstant;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.Pattern;
 
-public record FinancialTransactionCategoryCreateDTO(@NotBlank @Size(max = 30, message = "CATEGORY NAME TOO LONG")
-                                                    String name, @NotNull FinancialTransactionType type) {
+
+public record FinancialTransactionCategoryCreateDTO(
+        @NotBlank
+        @Pattern(regexp = RegexConstant.CATEGORY_NAME_PATTERN)
+        String name,
+        @NotNull FinancialTransactionType type,
+        @NotNull Long userId) {
 }
