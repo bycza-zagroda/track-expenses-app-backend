50dfadd6973be2b148b3c1c9447fd98add4122cc
iwona007 <iwonka.adamkiewicz@gmail.com>
2023-09-17 12:34:33 +0200

    uncomment baseIntegrationtests

--- src/test/java/pl/byczazagroda/trackexpensesappbackend/BaseIntegrationTestIT.java
+++ src/test/java/pl/byczazagroda/trackexpensesappbackend/BaseIntegrationTestIT.java
@@ -5,8 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -18,21 +22,21 @@ public abstract class BaseIntegrationTestIT {
 
     public static final String BEARER = "Bearer ";
 
-//    static final MySQLContainer<?> MY_SQL_CONTAINER;
+    static final MySQLContainer<?> MY_SQL_CONTAINER;
 
     static final int TESTCONTAINER_STARTUP_TIMEOUT = 480;
-//
-//    static {
-//        MY_SQL_CONTAINER =
-//                new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
-//                        .withDatabaseName("trackexpensesapptest")
-//                        .withUsername("root")
-//                        .withPassword("root")
-//                        .withStartupTimeoutSeconds(TESTCONTAINER_STARTUP_TIMEOUT)
-//                        .withCommand("--innodb-redo-log-capacity=32M")
-//                        .withUrlParam("serverTimezone", "UTC");
-//        MY_SQL_CONTAINER.start();
-//    }
+
+    static {
+        MY_SQL_CONTAINER =
+                new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
+                        .withDatabaseName("trackexpensesapptest")
+                        .withUsername("root")
+                        .withPassword("root")
+                        .withStartupTimeoutSeconds(TESTCONTAINER_STARTUP_TIMEOUT)
+                        .withCommand("--innodb-redo-log-capacity=32M")
+                        .withUrlParam("serverTimezone", "UTC");
+        MY_SQL_CONTAINER.start();
+    }
 
     @Autowired
     protected MockMvc mockMvc;
@@ -41,12 +45,12 @@ public abstract class BaseIntegrationTestIT {
     protected ObjectMapper objectMapper;
 
 
-//    @DynamicPropertySource
-//    private static void containerConfig(DynamicPropertyRegistry registry) {
-//        registry.add("spring.datasource.url", MY_SQL_CONTAINER::getJdbcUrl);
-//        registry.add("spring.datasource.username", MY_SQL_CONTAINER::getUsername);
-//        registry.add("spring.datasource.password", MY_SQL_CONTAINER::getPassword);
-//        registry.add("spring.datasource.driver-class-name", MY_SQL_CONTAINER::getDriverClassName);
-//        registry.add("spring.profiles.active", () -> "test");
-//    }
+    @DynamicPropertySource
+    private static void containerConfig(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", MY_SQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", MY_SQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", MY_SQL_CONTAINER::getPassword);
+        registry.add("spring.datasource.driver-class-name", MY_SQL_CONTAINER::getDriverClassName);
+        registry.add("spring.profiles.active", () -> "test");
+    }
 }
