e129feb58e9fadfc74ef434a31014a5d3e5d5c22
Dawid Bielecki <dawciobiel@gmail.com>
2024-01-27 19:24:50 +0100

    Expand WalletDTO javadoc documentation (#322)
    -add: add javadoc documentation for balance param

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/wallet/api/dto/WalletDTO.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/wallet/api/dto/WalletDTO.java
@@ -9,6 +9,7 @@ import java.time.Instant;
  * @param id is a technical identity number
  * @param name is a Wallet's name
  * @param creationDate is a date and hour when wallet has been created
+ * @param balance is the balance for the portfolio - the sum of income and expenses
  */
 public record WalletDTO(Long id, String name, Instant creationDate, Long userId, BigDecimal balance) {
 
